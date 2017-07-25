ee0b8b9877dbbfe25b360acc3605aa89a33ccf2b
Deepak Maurya <itsdpm@gmail.com>
2017-07-25 13:24:21 +0530

    Express servers with HTTP verbs mapped onto some database operation

--- node/node-express/server-3.js
+++ node/node-express/server-3.js
@@ -10,37 +10,38 @@ var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('./dishes', function(req, res, next){
+app.all('/dishes', function(req, res, next){
 	res.writeHead(200, {'Content_Type':'text/html'});
 	next();
 });
 
-app.get('./dishes', function(req, res, next){
+app.get('/dishes', function(req, res, next){
 	res.end('Sends all the dishes');
 });
 
-app.post('./dishes', function(req, res, next){
-	res.end('Adds the dish: '+req.body.name+' with details: '+res.body.description);
+app.post('/dishes', function(req, res, next){
+	res.end('Adds the dish: '+req.body.name+' with details: '+req.body.description);
 });
 
-app.delete('./dishes', function(req, res, next){
+app.delete('/dishes', function(req, res, next){
 	res.end('Deletes all the dishes!!');
 });
 
-app.get('./dishes', function(req, res, next){
+app.get('/dishes/:dishId', function(req, res, next){
 	res.end('Send the dish: '+req.params.dishId+' to you');
 });
 
-app.put('./dishes/:dishId', function(req, res, next){
+app.put('/dishes/:dishId', function(req, res, next){
 	res.write('Updating  the dish: '+req.params.dishId+'\n')
+	res.end('Will update the dish: '+req.body.name+' with details: '+req.body.description);
 });
 
-app.delete('./dishes/:dishId', function(req, res, next){
+app.delete('/dishes/:dishId', function(req, res, next){
 	res.end('Deleting dish: '+req.params.dishId);
 });
 
-app.use(express.static(__dirname+'./public'));
+app.use(express.static(__dirname+'/public'));
 
 app.listen(port, hostname, function(){
-	console.log('Server running at http://${hostname}:${port}/');
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
--- node/node-express/server-4.js
+++ node/node-express/server-4.js
@@ -11,45 +11,50 @@ app.use(morgan('dev'));
 
 var dishRouter = express.Router();
 
-dishRouter.use(bodyParser.json);
+dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-
-.all(function(req, res, next){
-	res.writeHead(200, {'Content_Type':'text/html'});
-	next();
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
 })
 
-.get(function(req, res, next){
-	res.end('Sends all the dishes');
+.get(function(req,res,next){
+        res.end('Will send all the dishes to you!');
 })
 
 .post(function(req, res, next){
-	res.end('Adds the dish: '+req.body.name+' with details: '+req.body.description);
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);    
 })
 
 .delete(function(req, res, next){
-	res.end('Deletes all the dishes!!');
+        res.end('Deleting all dishes');
 });
 
 dishRouter.route('/:dishId')
-
-.get('/:dishId', function(req, res, next){
-	res.end('Send the dish: '+req.params.dishId+' to you');
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
 })
 
-.put('/:dishId', function(req, res, next){
-	res.write('Updating  the dish: '+req.params.dishId+'\n')
+.get(function(req,res,next){
+        res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
 })
 
-.delete('/:dishId', function(req, res, next){
-	res.end('Deleting dish: '+req.params.dishId);
+.put(function(req, res, next){
+        res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + 
+            ' with details: ' + req.body.description);
+})
+
+.delete(function(req, res, next){
+        res.end('Deleting dish: ' + req.params.dishId);
 });
 
-app.use('/dishes', dishRouter);
+app.use('/dishes',dishRouter);
 
-app.use(express.static(__dirname+'./public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, hostname, function(){
-	console.log('Server running at http://${hostname}:${port}/');
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
