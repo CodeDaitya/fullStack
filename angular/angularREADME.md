bea23ad9f5e50f18e3e71d815c10eaea44b05b97
Deepak Maurya <itsdpm@gmail.com>
2017-11-19 12:43:58 +0530

    Dishdetail router path, Menu/Footer update

---
+++ angular/angularREADME.md
@@ -0,0 +1,1 @@
+# Angular Frontend Framework
--- angular/conFusion/src/app/app-routing/routes.ts
+++ angular/conFusion/src/app/app-routing/routes.ts
@@ -8,7 +8,8 @@ import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
 	{path: 'home', component: HomeComponent},
-	{path: 'menu', component:MenuComponent},
+	{path: 'menu', component: MenuComponent},
+	{path: 'dishdetail/:id', component: DishdetailComponent},
 	{path: 'contactus', component: ContactComponent},
 	{path: 'aboutus', component: AboutComponent},
 	{path: '', redirectTo: '/home', pathMatch: 'full'}
