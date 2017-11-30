51b8dfaf29bd6d7d0a5b37c6a55f4474d6161da1
Deepak Maurya <itsdpm@gmail.com>
2017-11-30 11:03:48 +0530

    Leader service, about component

--- angular/conFusion/src/app/app.module.ts
+++ angular/conFusion/src/app/app.module.ts
@@ -19,8 +19,10 @@ import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +33,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
 		BrowserModule,
@@ -44,7 +47,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   ],
   providers: [ 
 		DishService,
-		PromotionService
+		PromotionService,
+		LeaderService
 	 ],
   bootstrap: [AppComponent]
 })
