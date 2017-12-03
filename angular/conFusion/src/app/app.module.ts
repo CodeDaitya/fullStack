9a505c7be9c05bee2c01f6d4aa1575b6d9f9f1ca
Deepak Maurya <itsdpm@gmail.com>
2017-12-03 15:11:59 +0530

    Reactive forms

--- angular/conFusion/src/app/app.module.ts
+++ angular/conFusion/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import 'hammerjs';
 
@@ -43,7 +44,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 		BrowserAnimationsModule,
 		MaterialModule,
 		FlexLayoutModule,
-		AppRoutingModule
+		AppRoutingModule,
+		ReactiveFormsModule
   ],
   providers: [ 
 		DishService,
