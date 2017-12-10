17d57fc40afead5069ed3b0b35c963fcbf99928f
Deepak Maurya <itsdpm@gmail.com>
2017-12-10 12:29:41 +0530

    Promises with Angular, for available processed data

--- angular/conFusion/src/app/about/about.component.ts
+++ angular/conFusion/src/app/about/about.component.ts
@@ -14,7 +14,7 @@ export class AboutComponent implements OnInit {
 	constructor(private leaderservice: LeaderService) { }
 
   ngOnInit() {
-		this.leaders = this.leaderservice.getLeaders();
+		this.leaderservice.getLeaders().then(leader => this.leaders = leader);
   }
 
 }
--- angular/conFusion/src/app/services/promotion.service.ts
+++ angular/conFusion/src/app/services/promotion.service.ts
@@ -8,16 +8,16 @@ export class PromotionService {
 
 	constructor() { }
 	
-	getPromotions(): Promotion[] {
-		return PROMOTIONS;
+	getPromotions(): Promise<Promotion[]> {
+		return Promise.resolve(PROMOTIONS);
 	}
 
-	getPromotion(id: number): Promotion {
-		return PROMOTIONS.filter((promotion) => (promotion.id === id))[0];
+	getPromotion(id: number): Promise<Promotion> {
+		return Promise.resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]);
 	}
 
-	getFeaturedPromotion(): Promotion {
-		return PROMOTIONS.filter((promotion) => (promotion.featured))[0];
+	getFeaturedPromotion(): Promise<Promotion> {
+		return Promise.resolve(PROMOTIONS.filter((promotion) => (promotion.featured))[0]);
 	}
 
 }
