359aa620c375ae39302a3a9efcb7e9f5c2b860d2
Deepak Maurya <itsdpm@gmail.com>
2017-12-27 23:28:22 +0530

    Angular animations, visibility

--- angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
@@ -12,7 +13,20 @@ import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+	styleUrls: ['./dishdetail.component.scss']
+	animations: [
+		trigger('visibility', [
+			state('shown', style({
+				transform: 'scale(1.0)',
+				opacity: 1
+			})),
+			state('hidden', style({
+				transform: 'scale(0.5)',
+				opacity: 0
+			})),
+			transition('* => *', animate('0.5s ease-in-out'))
+		])
+	]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -24,6 +38,7 @@ export class DishdetailComponent implements OnInit {
 	commentForm: FormGroup;
 	comment: Comment;
 	errMess: string;
+	visibility = 'shown';
 
 	formErrors = {
 		'author': '',
@@ -52,8 +67,8 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-			.subscribe(dish => { this.dish = dish; this.dishCopy = dish; this.setPrevNext(dish.id); },
+      .switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']);})
+			.subscribe(dish => { this.dish = dish; this.dishCopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'},
 				errmess => this.errMess = <any>errmess );
 	}
 
