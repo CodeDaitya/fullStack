04cd07802d84434a8bdc0f5725552cfef66b1aef
Deepak Maurya <itsdpm@gmail.com>
2017-12-28 14:48:40 +0530

    Animations

--- angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -2,30 +2,21 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Comment } from '../shared/comment';
+import { visibility, expand } from '../animations/app.animations';
 
 import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-	styleUrls: ['./dishdetail.component.scss']
+	styleUrls: ['./dishdetail.component.scss'],
 	animations: [
-		trigger('visibility', [
-			state('shown', style({
-				transform: 'scale(1.0)',
-				opacity: 1
-			})),
-			state('hidden', style({
-				transform: 'scale(0.5)',
-				opacity: 0
-			})),
-			transition('* => *', animate('0.5s ease-in-out'))
-		])
+		visibility(),
+		expand()
 	]
 })
 export class DishdetailComponent implements OnInit {
