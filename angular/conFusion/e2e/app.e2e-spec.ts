baa256ad898ea685dd1a433d91fe257f17a202c0
Deepak Maurya <itsdpm@gmail.com>
2017-12-30 22:40:08 +0530

    End-to-end testing

--- angular/conFusion/e2e/app.e2e-spec.ts
+++ angular/conFusion/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('con-fusion App', () => {
   let page: AppPage;
@@ -8,7 +9,31 @@ describe('con-fusion App', () => {
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
+	});
+	
+	it('should navigate to about us page by clicking on its link', () => {
+		page.navigateTo('/');
+
+		let navlink = page.getAllElement('a').get(1);
+		navlink.click();
+
+		expect(page.getParagraphText('h3')).toBe('About Us');
+	});
+
+	it('should enter a new comment for the first dish', () => {
+		page.navigateTo('/dishdetail/0');
+
+		let newAuthor = page.getElement('input[type=text]');
+		newAuthor.sendKeys('Test Author');
+
+		let newComment = page.getElement('textarea');
+		newComment.sendKeys('Test Comment!!');
+
+		let newSubmitButton = page.getElement('button[type=submit]');
+		newSubmitButton.click();
+
+		browser.pause();
+	})
 });
